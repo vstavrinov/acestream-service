bce70795202da27dd6855353622b542414794b16
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2019-10-09 16:46:56 +0300

    - deploy script bug fix: moved release part (REST call) - deploy script bug fix: missing Authorization header in REST call - travis.yml bug fix:  wrong deploy script path

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_TAG" != "" ]; then
+if [ "$TRAVIS_TAG" = "" ]; then
     docker tag $DOCKER_USERNAME/$REPO registry.heroku.com/$REPO/web &&
     echo $HEROKU_API_KEY |
-    docker login -u $HEROKU_IDENTITY --password-stdin &&
-    docker push registry.heroku.com/$REPO/web
-else
-    echo $DOCKER_PASSWORD |
-    docker login -u $DOCKER_USERNAME --password-stdin &&
-    docker push $DOCKER_USERNAME/$REPO &&
+    docker login -u $HEROKU_IDENTITY --password-stdin registry.heroku.com &&
+    docker push registry.heroku.com/$REPO/web &&
     curl -n -X PATCH https://api.heroku.com/apps/$REPO/formation \
       -d '{
       "updates": [
@@ -18,6 +14,5 @@ else
         }
       ]
     }' \
-      -H "Content-Type: application/json" \
-      -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+      -H "Content-Type: application/json" \                                                                               -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \                                               -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY | base64)"                                      else                                                                                                                    echo $DOCKER_PASSWORD |                                                                                             docker login -u $DOCKER_USERNAME --password-stdin &&                                                                docker push $DOCKER_USERNAME/$REPO
 fi
