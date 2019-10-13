01342b6d48432499cca7c0c6b6307c87948f72f7
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2019-10-14 00:47:44 +0300

    rename docker repo and image

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_TAG" = "" ]; then
-    docker tag $DOCKER_USERNAME/$REPO registry.heroku.com/$REPO/web &&
+    docker tag $DOCKER_USERNAME/$DOCKER_REPO registry.heroku.com/$HEROKU_REPO/web &&
     echo $HEROKU_API_KEY |
     docker login -u $HEROKU_IDENTITY --password-stdin registry.heroku.com &&
-    docker push registry.heroku.com/$REPO/web &&
-    curl -n -X PATCH https://api.heroku.com/apps/$REPO/formation \
+    docker push registry.heroku.com/$HEROKU_REPO/web &&
+    curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_REPO/formation \
       -d '{
       "updates": [
         {
           "type": "web",
-          "docker_image": "'$(docker inspect $DOCKER_USERNAME/$REPO --format={{.Id}})'"
+          "docker_image": "'$(docker inspect $DOCKER_USERNAME/$HEROKU_REPO --format={{.Id}})'"
         }
       ]
     }' \
@@ -18,8 +18,8 @@ if [ "$TRAVIS_TAG" = "" ]; then
     -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
     -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY | base64)"
 else
-    docker tag $DOCKER_USERNAME/$REPO $DOCKER_USERNAME/$REPO:$TRAVIS_TAG &&
+    docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_TAG &&
     echo $DOCKER_PASSWORD |
     docker login -u $DOCKER_USERNAME --password-stdin &&
-    docker push $DOCKER_USERNAME/$REPO
+    docker push $DOCKER_USERNAME/$DOCKER_REPO
 fi
