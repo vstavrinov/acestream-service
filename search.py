804292117ac0ecd92a60a8f871379561f39468fc
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-07-16 08:23:01 +0300

    add port argument for python http server

--- search.py
+++ search.py
@@ -61,4 +61,8 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+    else:
+        port = 8088
+    app.run(debug=True, host='0.0.0.0', port=port)
