abd66f0eef053dd25959352a0dc0cdce6e3250d0
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-09-26 17:10:18 +0300

    echo vars

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ while read HEROKU_REPO HEROKU_IDENTITY HEROKU_API_KEY; do
     fi
 done
 # Deploy to docker hub new version (tag)
-echo Deploy to docker hub new version ${GITHUB_REF#refs/tags/}
+echo Deploy to docker hub new version GITHUB_REF=${GITHUB_REF#refs/tags/}, TAG=$TAG,  GITHUB_REF_NAME=$GITHUB_REF_NAME
 docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:${GITHUB_REF#refs/tags/} &&
 echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin &&
