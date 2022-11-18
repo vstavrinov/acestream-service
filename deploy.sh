510b2c946967182cf5c4572347dedfe36ce23a42
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-11-18 21:32:47 +0300

    add docker latest tag

--- deploy.sh
+++ deploy.sh
@@ -27,6 +27,8 @@ done
 echo Deploy to docker hub new version GITHUB_REF=${GITHUB_REF}, TAG=$TAG,  GITHUB_REF_NAME=$GITHUB_REF_NAME
 TAG=$(versioningit)
 docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:$TAG
+docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:latest
 echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin
 docker push $DOCKER_USERNAME/$DOCKER_REPO:$TAG
+docker push $DOCKER_USERNAME/$DOCKER_REPO:latest
