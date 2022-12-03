277aa2ec10ae7520c24690ef574e637a2c8cc85c
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-12-03 17:45:18 +0300

    remove heroku as closed

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,6 @@
 #!/bin/bash -e
 
 # Deploy to heroku and Cloud Function on commits of master branch 
-echo Deploy to heroku
-echo "$HEROKU" |
-while read HEROKU_REPO HEROKU_IDENTITY HEROKU_API_KEY; do
-    if [ -n "$HEROKU_REPO" ]; then
-        docker tag $DOCKER_USERNAME/$DOCKER_REPO registry.heroku.com/$HEROKU_REPO/web
-        echo $HEROKU_API_KEY |
-        docker login -u $HEROKU_IDENTITY --password-stdin registry.heroku.com
-        docker push registry.heroku.com/$HEROKU_REPO/web
-        curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_REPO/formation \
-         -d '{
-          "updates": [
-            {
-              "type": "web",
-              "docker_image": "'$(docker inspect registry.heroku.com/$HEROKU_REPO/web --format={{.Id}})'"
-            }
-          ]
-        }' \
-        -H "Content-Type: application/json" \
-        -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-        -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY | base64 -w0)"
-    fi
-done
 # Deploy to docker hub new version (tag)
 echo Deploy to docker hub new version GITHUB_REF=${GITHUB_REF}, TAG=$TAG,  GITHUB_REF_NAME=$GITHUB_REF_NAME
 TAG=$(versioningit)
